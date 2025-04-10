c92d4cac3db14120319a23ce1f8523556e98029b
Snizhana_K <snizhana.knysh@intellecteu.com>
2025-04-10 11:39:05 +0300

    refactor PrintPrimeNum

--- src/main/java/week1/day4/printPrimeNum/PrintPrimeNum.java
+++ src/main/java/week1/day4/printPrimeNum/PrintPrimeNum.java
@@ -7,57 +7,66 @@ import java.util.List;
 public class PrintPrimeNum {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(2,3,4,5,6,7,91,92,93);
+        List<Integer> numbers = Arrays.asList(2, 3, 4, 5, 6, 7, 91, 92, 93);
+        processInputNumbersList(numbers);
+    }
 
-        for (int i = 0; i < numbers.size(); i++) { // extract into separate method  everything that is now inside for-loop
-            int targetIndex;
-
-            if (numbers.get(i) > 9) {
-                String number = numbers.get(i).toString();
-                targetIndex = 0;
-                int sum = 0;
-
-                for (int j = 0; j < number.length(); j++) {
-                    targetIndex = sum + Character.getNumericValue(number.charAt(j));
-                    sum = targetIndex;
-                }
-            } else {
-                targetIndex = numbers.get(i);
-            }
-
-            boolean isNumberFound = false;
-            int currentPrimeIndex = 1;
-            int num = 2;
-
-            do
-            {                                                //extract something into separate method to avoid over-nesting
-                if (currentPrimeIndex == targetIndex) {
-                    System.out.print(num + " ");
-                    isNumberFound = true;
-                } else {
-                    num += 1;
-                    if (isPrime(num)) {
-                        currentPrimeIndex++;
-                    } else {
-                        do {
-                            num += 1;
-                        } while (!isPrime(num));
-                        currentPrimeIndex++;
-                    }
-                }
-            } while (!isNumberFound);
-
+    private static void processInputNumbersList(List<Integer> inputNumbers) {
+        for (Integer inputNumber : inputNumbers) {
+            int targetIndex = getTargetIndex(inputNumber);
+            printPrimeNumbers(targetIndex);
         }
     }
 
-    static boolean isPrime(int number) {
-            int counter = 0;
-            for (int i = 2; i <= number - 1; i++) {
-                if (number % i == 0) {
-                    counter++;
+    private static int getTargetIndex(int inputNumber) {
+        int targetIndex;
+
+        if (inputNumber > 9) {
+            String numberToDefineTargetIndex = String.valueOf(inputNumber);
+            targetIndex = 0;
+            int sum = 0;
+
+            for (int j = 0; j < numberToDefineTargetIndex.length(); j++) {
+                targetIndex = sum + Character.getNumericValue(numberToDefineTargetIndex.charAt(j));
+                sum = targetIndex;
+            }
+        } else {
+            targetIndex = inputNumber;
+        }
+
+        return targetIndex;
+    }
+
+    private static void printPrimeNumbers(int targetIndex) {
+        boolean isPrimeNumberFound = false;
+        int currentPrimeIndex = 1;
+        int currentNumber = 2;
+
+        do {
+            if (currentPrimeIndex == targetIndex) {
+                System.out.print(currentNumber + " ");
+                isPrimeNumberFound = true;
+            } else {
+                currentNumber += 1;
+                if (isPrime(currentNumber)) {
+                    currentPrimeIndex++;
+                } else {
+                    do {
+                        currentNumber += 1;
+                    } while (!isPrime(currentNumber));
+                    currentPrimeIndex++;
                 }
             }
-            return counter == 0;
+        } while (!isPrimeNumberFound);
 
+    }
+
+    static boolean isPrime(int number) {
+        for (int i = 2; i <= number - 1; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
